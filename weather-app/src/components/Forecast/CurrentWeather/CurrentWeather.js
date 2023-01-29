1a08205ef48a54e9b2e0f7745e99b011b5d2fd07
PlamDimitrov <plamen.danailov.dimitrov@gmail.com>
2023-01-29 22:56:53 +0200

    addition forcast information visualisation added

--- weather-app/src/components/Forecast/CurrentWeather/CurrentWeather.js
+++ weather-app/src/components/Forecast/CurrentWeather/CurrentWeather.js
@@ -6,6 +6,7 @@ import { selectedCityWeather } from '../../../store/citySlice';
 import WeatherDecoder from '../../../helpers/weatherDecoder';
 
 import styles from './CurrentWeather.module.css';
+import InfoIconSmall from '../../InfoIconSmall/InfoIconSmall';
 
 
 const CurrentWeather = ({ currentWeather, hourlyUnits }) => {
@@ -44,7 +45,37 @@ const CurrentWeather = ({ currentWeather, hourlyUnits }) => {
       </div>
       {weather.current_weather ? <h2>{WeatherDecoder(weather.current_weather.weathercode).description}</h2> : emptyCurrentWeatherData()}
       {weather.current_weather ? <h3>Apparent temperature {weather.hourly.apparent_temperature[getCurrentTimeIndex()]} <span>{weather.hourly_units.apparent_temperature}</span></h3> : emptyCurrentWeatherData()}
-      <div>
+      <div className={styles["additional-information"]}>
+        {weather.hourly
+          ? <InfoIconSmall {...{
+            type: "humidity",
+            value: weather.hourly.relativehumidity_2m[getCurrentTimeIndex()],
+            units: weather.hourly_units.relativehumidity_2m,
+          }} />
+          : null}
+        {weather.hourly
+          ? <InfoIconSmall {...{
+            type: "windSpeed",
+            value: weather.hourly.windspeed_10m[getCurrentTimeIndex()],
+            units: weather.hourly_units.windspeed_10m,
+          }} />
+          : null}
+        {weather.hourly
+          ? <InfoIconSmall {...{
+            type: "sunsrise",
+            value: weather.daily.sunrise[0].slice(-5),
+          }} />
+          : null}
+        {weather.hourly
+          ? <InfoIconSmall {...{
+            type: "sunset",
+            value: weather.daily.sunset[0].slice(-5),
+          }} />
+          : null}
+        {/* <h1>{weather.hourly ? JSON.stringify(weather.hourly.relativehumidity_2m[getCurrentTimeIndex()]) : null}</h1>
+        <h1>{weather.hourly ? JSON.stringify(weather.hourly.windspeed_10m[getCurrentTimeIndex()]) : null}</h1>
+        <h1>{weather.daily ? JSON.stringify(weather.daily.sunrise[0].slice(-5)) : null}</h1>
+        <h1>{weather.daily ? JSON.stringify(weather.daily.sunset[0].slice(-5)) : null}</h1> */}
       </div>
     </div>
   );
---
+++ weather-app/src/components/InfoIconSmall/InfoIconSmall.js
@@ -0,0 +1,66 @@
+import React, { useEffect } from 'react';
+
+import imgHumidity from '../../img/weather-icons/humidity.svg';
+import imgNotAvailable from '../../img/weather-icons/not-available.svg';
+import imgWindsock from '../../img/weather-icons/windsock.svg';
+import imgSunrise from '../../img/weather-icons/sunrise.svg';
+import imgSunset from '../../img/weather-icons/sunset.svg';
+
+import styles from './InfoIconSmall.module.css';
+
+
+const InfoIconSmall = (
+  { type = "",
+    value = "---",
+    units = "", }
+) => {
+
+  const typeMapper = () => {
+    switch (type) {
+      case "humidity":
+        return (
+          <>
+            <img src={imgHumidity} alt="humidity" />
+            <h3>{value} {units}</h3>
+          </>
+        )
+      case "windSpeed":
+        return (
+          <>
+            <img src={imgWindsock} alt="windspeed" />
+            <h3>{value} {units}</h3>
+          </>
+        )
+      case "sunsrise":
+        return (
+          <>
+            <img src={imgSunrise} alt="windspeed" />
+            <h3>{value} {units}</h3>
+          </>
+        )
+      case "sunset":
+        return (
+          <>
+            <img src={imgSunset} alt="windspeed" />
+            <h3>{value} {units}</h3>
+          </>
+        )
+
+      default:
+        return (
+          <>
+            <img src={imgNotAvailable} alt="windspeed" />
+            <h3>{value}</h3>
+          </>
+        )
+    }
+  }
+
+  return (
+    <div className={styles["info-icon-small"]}>
+      {typeMapper()}
+    </div>
+  )
+}
+
+export default InfoIconSmall;
